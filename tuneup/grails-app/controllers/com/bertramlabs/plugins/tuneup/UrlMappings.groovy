f7c9b5d057f160f0ceb6a39c70b368a7c88c4152
David Estes <davydotcom@gmail.com>
2017-11-30 10:06:39 -0600

    clean up url mappings

--- tuneup/grails-app/controllers/com/bertramlabs/plugins/tuneup/UrlMappings.groovy
+++ tuneup/grails-app/controllers/com/bertramlabs/plugins/tuneup/UrlMappings.groovy
@@ -1,7 +1,6 @@
 package com.bertramlabs.plugins.tuneup
 
 class UrlMappings {
-
     static mappings = {
         "/tuneup"(resources: 'slowTransactions', namespace: 'tuneup')
     }
---
+++ tuneup/src/main/groovy/com/bertramlabs/plugins/tuneup/MetricManager.groovy
@@ -0,0 +1,7 @@
+package com.bertramlabs.plugins.tuneup
+
+/**
+ *  @author David Estes
+ */
+class MetricManager {
+}
